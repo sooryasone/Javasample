59ffac245d374743382947a579e5f61b63c198a5
sooryasone <53096836+sooryasone@users.noreply.github.com>
2021-09-22 03:06:27 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ public class JavaProgram
     public static void main(String args[])
     {
    	for (int i=1;i<=3;i++{
-       System.out.println("Hello Xorainters! Welcome to the test session");
+       System.out.println("Hello Xorianters! Welcome to the test session");
 }
       
     }
